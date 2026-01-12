cbededed443a075a3c8de0ecebc8b8c04ba97050
Paul Sebastián Caceres Huisacayna <paul.caceresh@metrica-global.com>
2026-01-11 19:24:41 -0500

    Configurar despliegue con HashRouter y sin llaves API

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // 1. Importar HashRouter
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -11,6 +11,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Envolver App con HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
